cdd1f3ac7e47d910345185e8cb1363e7c7025ce8
santowanderson <wandersonsantospereira21@gmail.com>
2023-09-02 20:13:11 -0300

    submit logic started

---
+++ newsletter-sign-up/js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//subscribe page variables
+const subscribeContainer = document.querySelector('[data-subscribe-container]');
+const form = document.querySelector('[data-form]');
+const input = document.querySelector('[data-subscribe-input]');
+const subscribeButton = document.querySelector('[data-subscribe-button]');
+
+//subiscribed success page variables
+const subscribedContainer = document.querySelector('[data-subscribed-container]');
+const homePageButton = document.querySelector('[data-home-page-button]');
+
+
+function sendSubsciptionForm(element) {
+
+    element.preventDefault()
+
+    if (form.checkValidity) {
+        subscribeContainer.style.display = 'none';
+        subscribedContainer.style.display = 'block';
+
+        getInputValue();
+    }
+}
+
+function backToHomePage() {
+
+    subscribeContainer.style.display = 'block';
+    subscribedContainer.style.display = 'none';
+    input.value = '';
+}
+
+function getInputValue() {
+    const subscribedEmailInfo = document.querySelector('[data-subscribed-email]');
+
+    subscribedEmailInfo.innerHTML += input.value;
+}
+
+form.addEventListener('submit', sendSubsciptionForm);
+homePageButton.addEventListener('click', backToHomePage);
